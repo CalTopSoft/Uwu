8327a60fa157922ddd71447b0ba64c4f32b9decf
FlastXStudios <111031492+CalTopSoft@users.noreply.github.com>
2025-05-22 08:18:09 -0500

    Create bd.js

---
+++ bd.js
@@ -0,0 +1,5 @@
+export const casas = [
+  { id: 1, nombre: "Casa A", banos: 2, dormitorios: 3, precio: 50000, metros: 90 },
+  { id: 2, nombre: "Casa B", banos: 1, dormitorios: 2, precio: 35000, metros: 60 },
+  { id: 3, nombre: "Casa C", banos: 3, dormitorios: 4, precio: 75000, metros: 120 }
+];
